a50bf3909bd0420fa53b43b69d3b2b379ba84126
Andrew Lan <xila6676@colorado.edu>
2025-04-04 19:03:03 -0600

    part 4 almost working

--- Lab5/controllers/lab5_controller/lab5_controller.py
+++ Lab5/controllers/lab5_controller/lab5_controller.py
@@ -7,7 +7,11 @@ from matplotlib import pyplot as plt
 import heapq
 import time
 from scipy.signal import convolve2d # Uncomment if you want to use something else for finding the configuration space
-
+from ikpy.chain import Chain
+from ikpy.link import OriginLink, URDFLink
+import ikpy.utils.plot as plot_utils
+#from lab5_joint import getTargetFromObject, calculateIk, checkArmAtPosition, moveArmToTarget, reachArm, closeGrip, openGrip
+#import lab5_joint
 MAX_SPEED = 7.0  # [rad/s]
 MAX_SPEED_MS = 0.633 # [m/s]
 AXLE_LENGTH = 0.4044 # m
@@ -26,7 +30,6 @@ LIDAR_ANGLE_RANGE = math.radians(240)
 robot = Robot()
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
-
 # The Tiago robot has multiple motors, each identified by their names below
 part_names = ("head_2_joint", "head_1_joint", "torso_lift_joint", "arm_1_joint",
               "arm_2_joint",  "arm_3_joint",  "arm_4_joint",      "arm_5_joint",
@@ -36,6 +39,38 @@ part_names = ("head_2_joint", "head_1_joint", "torso_lift_joint", "arm_1_joint",
 # are controlled by a velocity controller. We therefore set their position to infinite.
 target_pos = (0.0, 0.0, 0.09, 0.07, 1.02, -3.16, 1.27, 1.32, 0.0, 1.41, 'inf', 'inf')
 robot_parts=[]
+
+base_elements=["base_link", "base_link_Torso_joint", "Torso", "torso_lift_joint", "torso_lift_link", "torso_lift_link_TIAGo front arm_11367_joint", "TIAGo front arm_11367"]
+my_chain = Chain.from_urdf_file("tiago_urdf.urdf", base_elements=["base_link", "base_link_Torso_joint", "Torso", "torso_lift_joint", "torso_lift_link", "torso_lift_link_TIAGo front arm_11367_joint", "TIAGo front arm_11367"])
+
+#print(my_chain.links)
+for link_id in range(len(my_chain.links)):
+
+    # This is the actual link object
+    link = my_chain.links[link_id]
+    
+    # I've disabled "torso_lift_joint" manually as it can cause
+    # the TIAGO to become unstable.
+    if link.name not in part_names or  link.name =="torso_lift_joint":
+        print("Disabling {}".format(link.name))
+        my_chain.active_links_mask[link_id] = False
+        
+# Initialize the arm motors and encoders.
+motors = []
+for link in my_chain.links:
+    if link.name in part_names and link.name != "torso_lift_joint":
+        motor = robot.getDevice(link.name)
+
+        # Make sure to account for any motors that
+        # require a different maximum velocity!
+        if link.name == "torso_lift_joint":
+            motor.setVelocity(0.07)
+        else:
+            motor.setVelocity(1)
+            
+        position_sensor = motor.getPositionSensor()
+        position_sensor.enable(timestep)
+        motors.append(motor)
 
 for i in range(N_PARTS):
     robot_parts.append(robot.getDevice(part_names[i]))
@@ -53,9 +88,9 @@ for _ in range (100):
 # The Tiago robot has a couple more sensors than the e-Puck
 # Some of them are mentioned below. We will use its LiDAR for Lab 5
 
-range = robot.getDevice('range-finder')
-range.enable(timestep)
-camera = robot.getDevice('camera')
+range_finder = robot.getDevice("RangeFinder")
+range_finder.enable(timestep)
+camera = robot.getDevice("camera")
 camera.enable(timestep)
 camera.recognitionEnable(timestep)
 lidar = robot.getDevice('Hokuyo URG-04LX-UG01')
@@ -94,9 +129,9 @@ lidar_offsets = lidar_offsets[83:len(lidar_offsets)-83] # Only keep lidar readin
 ##################### IMPORTANT #####################
 # Set the mode here. Please change to 'autonomous' before submission
 #mode = 'manual' # Part 1.1: manual mode
-mode = 'planner'
+#mode = 'planner'
 # mode = 'autonomous'
-# mode = 'picknplace'
+mode = 'picknplace'
 
 
 fix_map=False
@@ -108,8 +143,8 @@ fix_map=False
 if mode == 'planner':
     print("Planning route ...")
     # Part 2.3: Provide start and end in world coordinate frame and convert it to map's frame
-    # start_w = (-7.97,-4.84) # (Pose_X, Pose_Y) in meters
-    start_w=(-7.1,-5.3)
+    start_w = (-7.97,-4.84) # (Pose_X, Pose_Y) in meters
+    #start_w=(-7.1,-5.3)
     #test1: start_w = (-3, -3.3)
     #test2: start_w = (-8.5, -2)
     # while np.any(np.isnan(gps.getValues())):
@@ -245,7 +280,7 @@ if mode == 'planner':
 # Part 1.2: Map Initialization
 
 # Initialize your map data structure here as a 2D floating point array
-map = np.zeros(shape=[361,361])
+map = np.zeros(shape=[360,360])
 waypoints = []
 
 if mode == 'autonomous':
@@ -265,8 +300,180 @@ if mode == 'autonomous':
     #     # plt.scatter(path_x, path_y, c='black', marker='s')
     #     plt.plot(path_x,path_y, c='red', linewidth=2)
     # plt.savefig("path.png")
-
 state = 0 # use this to iterate through your path
+
+
+def getTargetFromObject(recognized_objects):
+    ''' Gets a target vector from a list of recognized objects '''
+
+    # Get the first valid target
+    target = recognized_objects[0].getPosition()
+
+    # Convert camera coordinates to IK/Robot coordinates
+    # offset_target = [-(target[2])+0.22, -target[0]+0.08, (target[1])+0.97+0.2]
+    offset_target = [-(target[2])+0.22, -target[0]+0.06, (target[1])+0.97+0.2]
+
+    return offset_target
+
+def lookForTarget(recognized_objects):
+    target_item = "orange"
+    if len(recognized_objects) > 0:
+
+        for item in recognized_objects:
+            if target_item in str(item.getModel()):
+
+                target = recognized_objects[0].getPosition()
+                dist = abs(target[2])
+
+                if dist < 5:
+                    return True
+                
+def checkArmAtPosition(ikResults, cutoff=0.00005):
+    '''Checks if arm at position, given ikResults'''
+    
+    # Get the initial position of the motors
+    initial_position = [0,0,0,0] + [m.getPositionSensor().getValue() for m in motors] + [0,0,0,0]
+
+    # Calculate the arm
+    arm_error = 0
+    for item in range(14):
+        arm_error += (initial_position[item] - ikResults[item])**2
+    arm_error = math.sqrt(arm_error)
+
+    if arm_error < cutoff:
+        if vrb:
+            print("Arm at position.")
+        return True
+    return False
+
+def calculateIk(offset_target,  orient=True, orientation_mode="Y", target_orientation=[0,0,1]):
+    '''
+    This will calculate the iK given a target in robot coords
+    Parameters
+    ----------
+    param offset_target: a vector specifying the target position of the end effector
+    param orient: whether or not to orient, default True
+    param orientation_mode: either "X", "Y", or "Z", default "Y"
+    param target_orientation: the target orientation vector, default [0,0,1]
+
+    Returns
+    ----------
+    rtype: bool
+        returns: whether or not the arm is at the target
+    '''
+    # # Get the number of links in the chain
+    # num_links = len(my_chain.links)
+
+    # # Create initial position array with the correct size
+    # initial_position = [0] * num_links
+    # #initial_position = [0, 0, 0, 0] + [m.getPositionSensor().getValue() for m in motors] +[0, 0, 0]
+
+    # # Map each motor to its corresponding link position
+    # motor_idx = 0
+    # for i in range(num_links):
+    #     link_name = my_chain.links[i].name
+    #     if link_name in part_names and link_name != "torso_lift_joint":
+    #         if motor_idx < len(motors):
+    #             initial_position[i] = motors[motor_idx].getPositionSensor().getValue()
+    #             motor_idx += 1
+
+    # # Calculate IK
+    # ikResults = my_chain.inverse_kinematics(
+    #     offset_target, 
+    #     initial_position=initial_position,
+    #     target_orientation=target_orientation, 
+    #     orientation_mode=orientation_mode
+    # )
+
+    # # Validate result
+    # position = my_chain.forward_kinematics(ikResults)
+    # squared_distance = math.sqrt(
+    #     (position[0, 3] - offset_target[0])**2 + 
+    #     (position[1, 3] - offset_target[1])**2 + 
+    #     (position[2, 3] - offset_target[2])**2
+    # )
+    # print(f"IK calculated with error - {squared_distance}")
+
+    # return ikResults
+
+    #Get the initial position of the motors
+    def bound_joint_values(initial_pos, bounds):
+        return [max(min(val, ub), lb) for val, (lb, ub) in zip(initial_pos, bounds)]
+    def get_bounded_initial_position():
+        initial_position = [0, 0, 0, 0] + [m.getPositionSensor().getValue() for m in motors] +[0, 0, 0]
+        bounds = [
+            (-float('inf'), float('inf')),  # 0
+            (-float('inf'), float('inf')),  # 1
+            (0.0, 0.35),                    # 2
+            (-float('inf'), float('inf')),  # 3
+            (0.07, 2.68),                   # 4 
+            (-1.5, 1.02),                   # 5 
+            (-3.46, 1.5),                   # 6
+            (-0.32, 2.29),                  # 7 
+            (-2.07, 2.07),                  # 8 
+            (-1.39, 1.39),                  # 9 
+            (-2.07, 2.07),                  # 10 
+            (-float('inf'), float('inf')),  # 11
+            (-float('inf'), float('inf')),  # 12
+            (0.0, 0.045),                   # 13
+        ]
+
+        return bound_joint_values(initial_position, bounds)
+    
+    initial_position = get_bounded_initial_position()
+    #initial_position = [0, 0, 0, 0] + [m.getPositionSensor().getValue() for m in motors] +[0, 0, 0] #
+    print(initial_position)
+    
+    #Calculate IK
+    ikResults = my_chain.inverse_kinematics(offset_target, initial_position=initial_position,  target_orientation = [0,0,1], orientation_mode="Y")
+
+    # Use FK to calculate squared_distance error
+    position = my_chain.forward_kinematics(ikResults)
+
+    # This is not currently used other than as a debug measure...
+    squared_distance = math.sqrt((position[0, 3] - offset_target[0])**2 + (position[1, 3] - offset_target[1])**2 + (position[2, 3] - offset_target[2])**2)
+    print("IK calculated with error - {}".format(squared_distance))
+
+    # Reset the ikTarget (deprec)
+    # ikTarget = offset_target
+    
+    return ikResults
+    
+    # Legacy code for visualizing
+        # import matplotlib.pyplot
+        # from mpl_toolkits.mplot3d import Axes3D
+        # ax = matplotlib.pyplot.figure().add_subplot(111, projection='3d')
+
+        # my_chain.plot(ikResults, ax, target=ikTarget)
+        # matplotlib.pyplot.show()
+vrb = True      
+def moveArmToTarget(ikResults):
+    '''Moves arm given ikResults'''
+    # Set the robot motors
+    for res in range(len(ikResults)):
+        if my_chain.links[res].name in part_names:
+            # This code was used to wait for the trunk, but now unnecessary.
+            # if abs(initial_position[2]-ikResults[2]) < 0.1 or res == 2:
+            robot.getDevice(my_chain.links[res].name).setPosition(ikResults[res])
+            if vrb:
+                print("Setting {} to {}".format(my_chain.links[res].name, ikResults[res]))
+
+def closeGrip():
+    robot.getDevice("gripper_right_finger_joint").setPosition(0.0)
+    robot.getDevice("gripper_left_finger_joint").setPosition(0.0)
+
+    # r_error = abs(robot.getDevice("gripper_right_finger_joint").getPositionSensor().getValue() - 0.01)
+    # l_error = abs(robot.getDevice("gripper_left_finger_joint").getPositionSensor().getValue() - 0.01)
+    
+    # print("ERRORS")
+    # print(r_error)
+    # print(l_error)
+
+    # if r_error+l_error > 0.0001:
+    #     return False
+    # else:
+    #     return True
+
 
 if mode == 'picknplace':
     # Part 4: Use the function calls from lab5_joints using the comments provided there
@@ -274,14 +481,23 @@ if mode == 'picknplace':
     ## do not change start_ws and end_ws below
     start_ws = [(3.7, 5.7)]
     end_ws = [(10.0, 9.3)]
-    #
-    #orange = camera.getRecognitionObjects()
-    #target_pos = getTargetFromObject(orange)
-    #ik_result = calculateIK(target_pos)
-    #reach = reachArm(target_pos, NONE, ik_result)
-    #if reach:
-    #   closeGrip()
-    #   
+    # start = world_to_map(start_ws[0], start_ws[1]) # (x, y) in 360x360 map
+    # end = world_to_map(end_ws[0], end_ws[1]) # (x, y) in 360x360 map
+    target = "orange"
+    while robot.step(timestep) != -1:
+        recognized_obj = camera.getRecognitionObjects()
+        # print("recognized:", recognized_obj)
+        if lookForTarget(recognized_obj) == True: 
+            target_pos = getTargetFromObject(recognized_obj)
+            # print("target_pos:", target_pos)
+            Ikresult = calculateIk(target_pos)
+            print("IK: ", Ikresult)
+            moveArmToTarget(Ikresult)
+            checkArmAtPosition(Ikresult)
+            #closeGrip()
+            
+            
+           
     pass
 robot_semi_state = 0
 run_start=True
@@ -398,22 +614,28 @@ while robot.step(timestep) != -1 and mode != 'planner':
         #     run_start = False
         # Part 3.2: Feedback controller
         #STEP 1: Calculate the error
-        x_goal, y_goal=waypoints[index]
-        rho=np.sqrt((x_goal - pose_x) ** 2 + (y_goal - pose_y) ** 2)  # Distance to the goal
-        theta_g = np.arctan2(y_goal - pose_y, x_goal - pose_x)  # Angle to the goal
-        alpha = theta_g - pose_theta  # Difference from robot's heading
-        if alpha<-np.pi:
-            alpha += 2*np.pi
-        if index < len(waypoints) - 1:
-            x_next, y_next = waypoints[index + 1]
-        else:
-            print("Goal reached")
-            vL=0
-            vR=0
-        # print("x_next:", x_next, "y_next: ", y_next)
-        print("rho:", rho, "theta_g: ", theta_g, "alpha: ", alpha)
-        print("x_goal: ", x_goal, "y_goal: ", y_goal)
-        print("index: ", index)
+        # x_goal, y_goal=waypoints[index]
+        # rho=np.sqrt((x_goal - pose_x) ** 2 + (y_goal - pose_y) ** 2)  # Distance to the goal
+        # theta_g = np.arctan2(y_goal - pose_y, x_goal - pose_x)  # Angle to the goal
+        # alpha = theta_g - pose_theta  # Difference from robot's heading
+        # if alpha<-np.pi:
+        #     alpha += 2*np.pi
+        # if index < len(waypoints) - 1:
+        #     x_next, y_next = waypoints[index + 1]
+        # else:
+        #     print("Goal reached")
+        #     vL=0
+        #     vR=0
+        # # print("x_next:", x_next, "y_next: ", y_next)
+        # print("rho:", rho, "theta_g: ", theta_g, "alpha: ", alpha)
+        # print("x_goal: ", x_goal, "y_goal: ", y_goal)
+        # print("index: ", index)
+        # # Normalize wheelspeed
+        # # (Keep the wheel speeds a bit less than the actual platform MAX_SPEED to minimize jerk)
+        # # vL=max(min(0.5*alpha+2*rho, MAX_SPEED-3),-MAX_SPEED+4)
+        # # vR=max(min(-0.5*alpha+2*rho, MAX_SPEED-3),-MAX_SPEED+4)
+        # vL=max(min(2.5*alpha+6*rho, MAX_SPEED),-MAX_SPEED)
+        # vR=max(min(-2.5*alpha+6*rho, MAX_SPEED),-MAX_SPEED)
         # theta_goal_orientation = np.arctan2(y_next - y_goal, x_next - x_goal)
         # eta = theta_goal_orientation - pose_theta
         # eta = (eta + np.pi) % (2 * np.pi) - np.pi
@@ -466,12 +688,19 @@ while robot.step(timestep) != -1 and mode != 'planner':
         #                 vR=0
         #             robot_semi_state = 0
         print("vL:", vL, "vR: ", vR)
-        if rho<0.25: 
-            if index < len(waypoints) - 1: index += 1
-            else: 
-                print("Goal reached")
+        print("dx: ", dx, "dy: ", dy)
+        if rho < 0.1:
+            state += 1
+            if state >= len(waypoints):
+                state = 0
                 vL=0
                 vR=0
+        # if rho<0.25: 
+        #     if index < len(waypoints) - 1: index += 1
+        #     else: 
+        #         print("Goal reached")
+        #         vL=0
+        #         vR=0
         # vL = 0
         # vR = 0
 
@@ -481,7 +710,7 @@ while robot.step(timestep) != -1 and mode != 'planner':
     # pose_y -= (vL+vR)/2/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0*math.sin(pose_theta)
     # pose_theta += (vR-vL)/AXLE_LENGTH/MAX_SPEED*MAX_SPEED_MS*timestep/1000.0
 
-    print("X: %f Z: %f Theta: %f" % (pose_x, pose_y, pose_theta))
+    #print("X: %f Z: %f Theta: %f" % (pose_x, pose_y, pose_theta))
 
     # Actuator commands
     robot_parts[MOTOR_LEFT].setVelocity(vL)
